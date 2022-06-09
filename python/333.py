d93cf7f843a9c1b27ea43c56bc3ddc4778f0a18a
Bobla-2 <103276078+Bobla-2@users.noreply.github.com>
2022-06-09 20:37:24 +0300

    Update 333.py
    питон не робит нормально

--- python/333.py
+++ python/333.py
@@ -1,18 +1,23 @@
 import tkinter as tk
-
+from tkinter import *
 x = 0
 
 def rrrer():
     global x
     x = listbox.curselection()
     res2.configure( text = "%s" % x)
+    
 top = tk.Tk()
 top.title("артем лох")
 top['bg']="#51E1D1"
-top.geometry("300x250")
-lbl = tk.Label(top, text="My Favourite Fruits",bg="Orange",width="200").pack()
-listbox = tk.Listbox(top)
+top.geometry("300x350")
 
+frame = Frame(top)
+frame1 = Frame(top)
+frame2 = Frame(frame)
+frame3 = Frame(frame)
+lbl = tk.Label(top, text="My Favourite Fruits",bg="Orange",width="200").pack()
+listbox = tk.Listbox(frame2)
 
 str1 = "dfsghbdf"
 str2 = "yyydfg"
@@ -23,13 +28,21 @@ listbox.insert(2, str2)
 listbox.insert(3, str3)
 listbox.insert(4, str4)
 
-btn = tk.Button(top, text="delete",bg="orange" ,command=rrrer )
+
+closeButton = tk.Button(top, text="открыть",command=rrrer)
 res2 = tk.Label(top, text = "-")
-listbox.pack(pady=5)
-btn.pack()
-res2.pack()
+frame.pack()
+frame2.pack(side = LEFT)
+frame3.pack(side = LEFT, expand=True)
+#btn = tk.Button(frame, text="delete",bg="orange" ,command=rrrer )
 
+listbox.pack(side = LEFT)
+#btn.pack(side = RIGHT)
 
+frame1.pack(expand=True)
+ #fill=BOTH 
+closeButton.pack(side=LEFT, padx=5, pady=5)
+res2.pack(side=LEFT)
 #x = listbox.curselection()
 #//print(x)
 
