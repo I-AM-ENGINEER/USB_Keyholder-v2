fc99b839fe95bd985b869da452c2e90288ffac8d
Bobla-2 <savva050203@gmail.com>
2022-12-08 15:23:47 +0300

    Update ui.py

--- python/pypr/ui.py
+++ python/pypr/ui.py
@@ -9,7 +9,7 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-
+QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
