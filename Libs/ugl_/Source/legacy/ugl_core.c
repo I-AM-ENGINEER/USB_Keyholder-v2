082dd00487f94d44d5754d6b40f422d56995433b
I-AM-ENGINEER <sdwad6@gmail.com>
2023-12-07 16:02:31 +0300

    ugl lib dependance fix

---
+++ Libs/ugl_/Source/legacy/ugl_core.c
@@ -0,0 +1,48 @@
+#include "ugl_core.h"
+
+ugl_menu_t *UI_current_menu = NULL;
+
+void ugl_return( void ){
+	if(UI_current_menu->caller_ptr == NULL) return;
+	ugl_menu_t *returnMenu = UI_current_menu->caller_ptr;
+	ugl_menu_destructor(UI_current_menu);
+	UI_current_menu = returnMenu;
+}
+
+void ugl_enter( int32_t ID, ugl_menu_t* (*menu_constructor) ( int32_t ID, void* extra ), void* extra ){
+	ugl_menu_t *oldMenu = UI_current_menu;
+	ugl_menu_t *curMenu = menu_constructor(ID, extra);
+	if(curMenu == NULL) return;
+	UI_current_menu = curMenu;
+	UI_current_menu->caller_ptr = oldMenu;
+}
+
+ugl_menu_t *ugl_get_current_menu( void ){
+	return UI_current_menu;
+}
+
+ugl_menu_t *ugl_get_menu_by_id( uint32_t ID ){
+	ugl_menu_t* menu = UI_current_menu;
+	while(menu != NULL){
+		if(menu->ID == ID){
+			return menu;
+		}
+		menu = menu->caller_ptr;
+	}
+	return NULL;
+}
+
+void ugl_process( void ){
+	if(UI_current_menu->process_f != NULL){
+		UI_current_menu->process_f();
+	}
+}
+
+void ugl_render( void ){
+	if(UI_current_menu->process_f != NULL){
+		UI_current_menu->process_f();
+	}
+	if(UI_current_menu->render_f != NULL){
+		UI_current_menu->render_f();
+	}
+}
